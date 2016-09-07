2f319ec55101d7c94ad3f42e4c3c027445807b2d
AndyLi2020 <Xiaomei@encompass8.cn>
2016-09-07 13:47:58 +0800

    Initial Commit

---
+++ OpenCVDemo7/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenCVDemo7
+//
+//  Created by Xiaomei LI on 9/7/16.
+//  Copyright © 2016 Xiaomei LI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
